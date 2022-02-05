8bb51486ed784385e8a70df342ca5b413a92c26f
Sarah Leong <sleong20@stuy.edu>
2022-02-05 12:06:24 -0500

    yay random pixels

--- start.py
+++ start.py
@@ -1,4 +1,20 @@
+import random
+
 def main() :
-    print("how do you do?\n")
+    # print("how do you do?\n")
+
+    f = open("image0.ppm", "w")
+    f.write("P3\n500 500\n")
+    for i in range(500):
+        for j in range(500):
+            r = random.randint(0,256)
+            g = random.randint(0,256)
+            b = random.randint(0,256)
+            # r = 100
+            # g = 200
+            # b = 150
+            f.write(str(r) + " " + str(g) + " " + str(b) + " ")
+        f.write("\n")
+    f.close()
 
 main()
